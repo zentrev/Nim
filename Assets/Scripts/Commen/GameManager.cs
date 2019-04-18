8d7046900582cca70318bd3934cb8a1038fd04d7
Thumpax <c.t.raines.iii@gmail.com>
2019-04-18 09:58:43 -0600

    nims a bit

---
+++ Assets/Scripts/Commen/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public enum eDifficlty
+    {
+        INVALID,
+        EASY,
+        NORMAL,
+        HARD,
+    }
+
+    public int m_nimsObjects = 0;
+
+    public void StartGame(eDifficlty difficulty)
+    {
+        switch (difficulty)
+        {
+            case eDifficlty.EASY:
+                m_nimsObjects = 6;
+                break;
+            case eDifficlty.NORMAL:
+                m_nimsObjects = 12;
+                break;
+            case eDifficlty.HARD:
+                m_nimsObjects = 187;
+                break;
+            default:
+                Debug.LogError("INVALID DIFFICULTY");
+                break;
+        }
+    }
+
+    public void ChangeObjectCount(int count)
+    {
+        m_nimsObjects -= count;
+    }
+}
