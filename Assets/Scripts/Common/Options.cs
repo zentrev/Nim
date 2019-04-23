ce99a72ca2ea0165978d64346ecba5b8df3bad58
Remy <eremy@student.neumont.edu>
2019-04-23 17:55:36 -0600

    changed options and score menus

--- Assets/Scripts/Common/Options.cs
+++ Assets/Scripts/Common/Options.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Options : MonoBehaviour
+public class Options : Window
 {
     [SerializeField] AudioManager m_audioManager = null;
+    [SerializeField] GameObject panel = null;
 
     public void MuteMusic()
     {
         //AudioManager.Instance.GetComponent.
+    }
+
+    public void Update()
+    {
+        if (IsActive)
+        {
+            panel.SetActive(true);
+
+        }
+        else
+        {
+            panel.SetActive(false);
+        }
+        
     }
 }
