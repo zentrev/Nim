357155763abf4486e6bd8c6307d496e3c63dc73f
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2019-05-14 10:59:18 -0600

    ADS

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,14 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : Singleton<AdManager>
+{
+    public InterstitialAd myAd = null;
+    public int gameCount = 0;
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AdScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdScript : MonoBehaviour
+{
+    InterstitialAd myAd = null;
+    private void Start()
+    {
+        AdScript[] temp = GameObject.FindObjectsOfType<AdScript>();
+
+        if(temp.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        string appId = "ca-app-pub-8549494723516066~2991453367";
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+        InterstitialAd myAd = new InterstitialAd("ca-app-pub-8549494723516066/2406319370");
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadAd()
+    {
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        myAd.LoadAd(request);
+    }
+
+    public void ShowAd()
+    {
+        if (myAd.IsLoaded())
+        {
+            myAd.Show();
+        }
+        LoadAd();
+    }
+
+}
